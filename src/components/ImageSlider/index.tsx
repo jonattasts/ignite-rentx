a954eb3f8adc2dc8456ed77e338200be6453afb8
jonatasts <jonatassantos96@hotmail.com>
2023-03-17 15:33:56 -0300

    ✨ Create image slider component

---
+++ src/components/ImageSlider/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FlatList } from "react-native";
+import {
+  CarImage,
+  CarImageWrapper,
+  Container,
+  ImageIndex,
+  ImageIndexes,
+} from "./styles";
+
+interface Props {
+  imagesUrl: string[];
+}
+
+export function ImageSlider({ imagesUrl }: Props) {
+  return (
+    <Container>
+      <ImageIndexes>
+        <ImageIndex active={true} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+      </ImageIndexes>
+
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={(item) => String(Math.random())}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage
+              source={{
+                uri: imagesUrl[0],
+              }}
+              resizeMode="contain"
+            />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </Container>
+  );
+}
