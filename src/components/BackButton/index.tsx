031c16bd52bb760e8596e4e8f214d5d96f2e06f2
jonatasts <jonatassantos96@hotmail.com>
2023-03-17 10:24:40 -0300

    ✨ Create back button component

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { useTheme } from 'styled-components';
+
+
+import {
+  Container
+} from './styles';
+
+interface Props {
+  color?: string;
+  onPress: () => void;
+}
+
+export function BackButton({ color, onPress, ...rest }: Props) {
+  const theme = useTheme();
+
+  return (
+    <Container onPress={onPress}>
+      <MaterialIcons
+        name="chevron-left"
+        size={24}
+        color={color ? color : theme.colors.text}
+      />
+    </Container>
+  );
+}
