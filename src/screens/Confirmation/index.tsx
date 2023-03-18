eaa40701cd5eaa54b18c05be7eea77ddf1f9affe
jonatasts <jonatassantos96@hotmail.com>
2023-03-18 15:30:18 -0300

    ✨ Create confirmation screen

---
+++ src/screens/Confirmation/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useWindowDimensions, StatusBar } from "react-native";
+
+import LogoSvg from "../../assets/logo_background_gray.svg";
+import DoneSvg from "../../assets/done.svg";
+
+import { ConfirmButton } from "../../components/ConfirmButton";
+
+import { Container, Content, Title, Message, Footer } from "./styles";
+
+interface Params {
+  title: string;
+  message: string;
+  nextScreenRoute: string;
+}
+
+export function Confirmation() {
+  const { width } = useWindowDimensions();
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <LogoSvg width={width} />
+
+      <Content>
+        <DoneSvg width={80} height={80} />
+        <Title>Carro alugado!</Title>
+
+        <Message>
+          Agora você só precisa ir {"\n"}
+          até a concessionária da RENTX {"\n"}
+          pegar seu automóvel.
+        </Message>
+      </Content>
+
+      <Footer>
+        <ConfirmButton title="OK" onPress={() => {}} />
+      </Footer>
+    </Container>
+  );
+}
