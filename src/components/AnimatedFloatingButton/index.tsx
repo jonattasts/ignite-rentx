e3ccbfd40cba2a45a3844fbb90f1d610a26f60c8
jonatasts <jonatassantos96@hotmail.com>
2023-03-31 16:25:42 -0300

    🔥 Remove unnecessary code

--- src/components/AnimatedFloatingButton/index.tsx
+++ src/components/AnimatedFloatingButton/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import Animated, {
-  Extrapolate,
-  interpolate,
   useAnimatedGestureHandler,
   useAnimatedStyle,
   useSharedValue,
