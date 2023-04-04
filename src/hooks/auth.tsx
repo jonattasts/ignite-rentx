aea58e68760cde8e04309baf76a99e3935b7f025
jonatasts <jonatassantos96@hotmail.com>
2023-04-03 21:40:25 -0300

    ✨ Load user data from database

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,7 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
+
+import { database } from "../database";
+import { User as ModelUser } from "../database/model/User";
 
 import { api } from "../services/api";
 
@@ -42,11 +45,42 @@ function AuthProvider({ children }: AuthProviderProps) {
 
       api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
+      const userCollection = database.get<ModelUser>("users");
+
+      await database.action(async () => {
+        await userCollection.create((newUser) => {
+          (newUser.user_id = user.id),
+            (newUser.name = user.name),
+            (newUser.email = user.email),
+            (newUser.driver_license = user.driver_license),
+            (newUser.avatar = user.avatar),
+            (newUser.token = token);
+        });
+      });
+
       setData({ ...user, token });
     } catch (error: any) {
       throw new Error(error);
     }
   }
+
+  useEffect(() => {
+    async function loadUserData() {
+      const userCollection = database.get<ModelUser>("users");
+      const response = await userCollection.query().fetch();
+
+      if (response.length > 0) {
+        const userData = response[0]._raw as unknown as User;
+
+        api.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${userData.token}`;
+        setData(userData);
+      }
+    }
+
+    loadUserData();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data }}>
